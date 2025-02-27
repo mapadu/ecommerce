3ce2a079b09fdfd87f4780cf6b41133a2add6639
mapadu <rajmundtakacs@gmail.com>
2025-02-27 13:01:55 +0100

    Connecting the server to database and making a gitignore and env file.

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const app = express();
-
 const PORT = 3000;
+const { query } = require('./db');
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello, this is an ecommerce server');
+});
+
+app.get('/users', async (req,res) => {
+    try {
+        const result = await query('SELECT * FROM users');
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error fetching users.')
+    }
 });
 
 app.listen(PORT, () => {
