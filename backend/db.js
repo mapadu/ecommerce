caabfa3744cba8140ace73b33caef40b8293a298
mapadu <rajmundtakacs@gmail.com>
2025-07-23 16:38:07 +0200

    Using DATABASE_URL for Render deployment

--- backend/db.js
+++ backend/db.js
@@ -4,14 +4,7 @@ const logger = require('./utils/logger');
 const { Pool } = require('pg');
 
 const pool = new Pool ({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    max: 20,                  
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
+    connectionString: process.env.DATABASE_URL,
     ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
